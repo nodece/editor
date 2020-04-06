6d7a9a326bf9427931a003e6a3795c96b73d4379
Zixuan Liu <nodeces@gmail.com>
2020-04-06 21:03:13 +0800

    Updates

---
+++ precache-manifest.77be83c1b806a9ed961b3d0928b5d9ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b08ae092a90f0e08c61893b1b58de599",
+    "url": "/casbin-editor/index.html"
+  },
+  {
+    "revision": "0dead184624410fe97ed",
+    "url": "/casbin-editor/static/css/2.97ca6895.chunk.css"
+  },
+  {
+    "revision": "0dead184624410fe97ed",
+    "url": "/casbin-editor/static/js/2.199c058e.chunk.js"
+  },
+  {
+    "revision": "ac5b259a50ebc184d018b62300afe096",
+    "url": "/casbin-editor/static/js/2.199c058e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e473a2eec588ea2eb6b",
+    "url": "/casbin-editor/static/js/main.8d21323b.chunk.js"
+  },
+  {
+    "revision": "d673d6c240d2b4b195da",
+    "url": "/casbin-editor/static/js/runtime-main.c04abaec.js"
+  }
+]);
